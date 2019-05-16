ebed382133be6fb916d5066b6c693e419d4d04a3
matthew-laine <matthew.i.laine@gmail.com>
2019-05-15 22:42:49 -0400

    all iterations + stretch task done

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,13 +1,23 @@
+import java.util.Random;
 
 public class GuessingGame {
+	private static final String YOU_WIN = "You Win!";
+	private static final String TOO_HIGH = "Too High, Guess Again...";
+	private static final String TOO_LOW = "Too Low, Guess Again...";
+	private static final String INSTRUCTIONS = "Guess a number from 1 to 10.\n"
+			+ "Enter -1 to quit, and 0 for instructions.";
+	Random random = new Random();
+	int correctNumber = random.nextInt(10) + 1;
+
 	public String playGame(int guessedNumber) {
 		if(guessedNumber == 0) {
-			return "Guess a number from 1 to 10.";
-		}else if(guessedNumber < 7) {
-			return "Too Low, Guess Again...";
-		}else if(guessedNumber > 7) {
-			return "Too High, Guess Again...";
+			return INSTRUCTIONS;
+		}else if(guessedNumber < correctNumber) {
+			return TOO_LOW;
+		}else if(guessedNumber > correctNumber) {
+			return TOO_HIGH;
+		}else {
+			return YOU_WIN;
 		}
-		return "You Win!";
 	}
 }
--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,6 +1,51 @@
+import java.util.Scanner;
 
 public class GuessingGameApp {
-	GuessingGame myGame = new GuessingGame();
-	
-	
+	GuessingGame myGuessingGame = new GuessingGame();
+	Scanner myScanner = new Scanner(System.in);
+	private static final String YOU_WIN = "You Win!";
+	private static final String TOO_HIGH = "Too High, Guess Again...";
+	private static final String TOO_LOW = "Too Low, Guess Again...";
+	private static final String INSTRUCTIONS = "Guess a number from 1 to 10.\n"
+									+ "Enter -1 to quit, and 0 for instructions.";
+	int attemptsLeft = 3;
+
+	public void runGuessingGameApp() {
+		boolean gameIsStillGoingOn = true;
+		System.out.println(INSTRUCTIONS);
+		int gameInput;
+		String gameOutput;
+		
+		while(gameIsStillGoingOn) {
+			gameInput = myScanner.nextInt();
+			gameOutput = myGuessingGame.playGame(gameInput);
+			if(gameInput == -1) {
+				System.out.println("Good Bye!");
+				break;
+			}
+			if(gameOutput == INSTRUCTIONS) {
+				System.out.println(gameOutput);
+			} else if(gameOutput == TOO_HIGH || gameOutput == TOO_LOW) {
+				if(attemptsLeft > 1) {
+					System.out.println(gameOutput);
+					attemptsLeft--;
+					System.out.println(attemptsLeft + " attempts left");
+					if(attemptsLeft <= 0) {
+						gameIsStillGoingOn = false;
+					}
+				} else {
+					System.out.println("Sorry, You Lose :(");
+					gameIsStillGoingOn = false;
+				}
+			} else if(gameOutput == YOU_WIN) {
+				System.out.println(gameOutput);
+				gameIsStillGoingOn = false;
+			} else {
+				System.out.println("gameOutput != predetermined values\n" +
+							"program will now exit");
+				break;
+			}
+		}
+	}
+
 }
